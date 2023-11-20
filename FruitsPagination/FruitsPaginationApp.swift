9555b5867cfb5da19faef44ca489586be76a98c6
Nilam <shahnilam812@gmail.com>
2023-11-20 14:06:08 +0530

    Initial Commit

---
+++ FruitsPagination/FruitsPaginationApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsPaginationApp.swift
+//  FruitsPagination
+//
+//  Created by Nilam Shah on 20/11/23.
+//
+
+import SwiftUI
+
+@main
+struct FruitsPaginationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
